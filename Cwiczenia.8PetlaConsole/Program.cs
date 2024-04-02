274d50dcbb151eee7e89b901848871f81ac5204f
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-02 20:30:36 +0200

    Add project files.

---
+++ Cwiczenia.8PetlaConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Wprowadź ciąg znaków:");
+		string tekst = Console.ReadLine();
+		string odwroconyTekst = OdwrocCiag(tekst);
+		Console.WriteLine("Odwrocony ciąg znaków: " + odwroconyTekst);
+	}
+
+	static string OdwrocCiag(string tekst)
+	{
+		char[] tablicaZnakow = tekst.ToCharArray();
+		Array.Reverse(tablicaZnakow);
+		return new string(tablicaZnakow);
+	}
+}
